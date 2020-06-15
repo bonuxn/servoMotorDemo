830c757c6290c3ccbd92d9cb77db9fd7be276213
bonuxn <delacs1739@gmail.com>
2020-06-15 19:04:02 +0900

    servo test code

--- servo_driver.py
+++ servo_driver.py
@@ -1,11 +1,19 @@
-import wiringpi #GPIO制御ライブラリ
-import time #タイマライブラリ
-import sys
+import time
+import RPi.GPIO as GPIO
 
+GPIO.setmode(GPIO.BCM)
 
-class Servo:
+gp_out = 4
+GPIO.setup(gp_out, GPIO.OUT)
+servo = GPIO.PWM(gp_out, 50) 
 
-	def __init__(self,pin):
-		self._pin = pin
-	
-	def 
+servo.start(0.0)
+
+for i in range(10):
+	servo.ChangeDutyCycle(2.5)
+	time.sleep(0.5)
+
+	servo.ChangeDutyCycle(12.0)
+	time.sleep(0.5)
+
+GPIO.cleanup()
